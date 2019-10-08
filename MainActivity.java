32e2a58866600b8510b9a6337bf7a114e9647a5b
Vadim Timofeev <timofeev@iskra-kkm.ru>
2019-10-08 16:51:25 +0300

    Добавил MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.refresh.pos.nativecpp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        String txt = stringFromJNI();
+        String dat = dataFromJNI();
+
+        tv.setText(txt + dat);
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public native String dataFromJNI();
+}
